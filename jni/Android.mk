b0738c05e1c64ef32746567fae8d97bebef61e54
sunday <sun@chengcheng.info>
2015-03-10 22:41:05 +0800

    add ndk executable.

---
+++ jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := a.out
+LOCAL_SRC_FILES := a.c
+include $(BUILD_EXECUTABLE)
+
---
+++ jni/a.c
@@ -0,0 +1,11 @@
+/*
+ * Linux 备份程序，远程备份到目的地。
+ */
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    printf("HELLO,WORLD\n");
+    return 0;
+}
+
